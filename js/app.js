46639c3ab40c610f8792242baf93bc011c6b8adf
eckermania <erineckerman@gmail.com>
2018-08-14 21:57:38 -0700

    completed css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+prompt('Enter password:');
+
 // Time slots
 var time = ['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm'];
 
@@ -120,3 +122,4 @@ function renderAllStores() {
 
 makeHeaderRow();
 renderAllStores();
+
